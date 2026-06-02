2b8622126e8a7fd726d0fcc2ca7486fd0ad8fc3f
permana10 <arief2092@gmail.com>
2026-06-02 20:34:52 +0700

    Fix Gradle repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "SKEYPRO-NotificationListener"
+
 include(":app")
